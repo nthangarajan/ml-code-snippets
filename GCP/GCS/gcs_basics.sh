c457eceb06cf4bfb34dd424aa659a05a940a6ff4
Thangarajan Nagarethinam <nthangarajan@gmail.com>
2022-05-30 16:24:01 +0800

    Draft

---
+++ GCP/GCS/gcs_basics.sh
@@ -0,0 +1,20 @@
+#Creating a bucket named packt-gcp:
+gsutil mb gs://packt-gcp
+
+#Uploading a file to the bucket:
+gsutil cp gs://packt-gcp/
+
+#Creating a subfolder in the bucket:
+gsutil cp your-file gs://packt-gcp/
+
+#Listing the folder:
+gsutil ls gs://packt-gcp/
+
+#Getting help on gsutil commands:
+gsutil help
+
+#How much storage are we using (the -h makes it readable):
+gsutil du -h gs://packt-gcp/
+
+#Copying a whole folder to a bucket:
+gsutil cp -r gs://packt-gcp/
