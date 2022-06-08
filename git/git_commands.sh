b743e8b4d4534696e167bbd52486c05d24964b45
Thangarajan Nagarethinam <nthangarajan@gmail.com>
2022-06-08 16:20:50 +0800

    more snippets

---
+++ git/git_commands.sh
@@ -0,0 +1,8 @@
+git clone -b master https://bitbucket.micron.com/bbdc/scm/bdct/atap-analyticmodules.git
+
+
+
+git clone -b cicd-implementation https://bitbucket.micron.com/bbdc/scm/bdct/atap-analyticpipeline.git
+
+
+git clone -b bugfix/WWDS-4233-missing-equipment-from-atap-dashboard https://bitbucket.micron.com/bbdc/scm/bdct/atap-analyticmodules.git
