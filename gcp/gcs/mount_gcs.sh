6856639673b32d6777b05af87149a78d76d3baf1
Thangarajan Nagarethinam <nthangarajan@gmail.com>
2022-06-10 10:44:40 +0800

    more snippets

--- gcp/gcs/mount_gcs.sh
+++ gcp/gcs/mount_gcs.sh
@@ -10,12 +10,12 @@ export MY_BUCKET=$1
 
 cd ~/ # This should take you to /home/jupyter/
 
-mkdir -p gcs # Create a folder that will be used as a mount point
+mkdir -p gcs/$MY_BUCKET # Create a folder that will be used as a mount point
 
 gcsfuse --implicit-dirs \
 --rename-dir-limit=100 \
 --disable-http2 \
 --max-conns-per-host=100 \
-$MY_BUCKET "/home/jupyter/gcs"
+$MY_BUCKET "/home/jupyter/gcs/${MY_BUCKET}"
 
-echo "${MY_BUCKET} mounted with /home/jupyter/gcs"
+echo "${MY_BUCKET} mounted with /home/jupyter/gcs/${MY_BUCKET}"
--- gcp/gcs/unmount_gcs.sh
+++ gcp/gcs/unmount_gcs.sh
@@ -1,2 +1,13 @@
-fusermount -u /home/jupyter/gcs/
-rmdir /home/jupyter/gcs/
+if [ -z "$1" ]
+then
+    echo "please enter bucket name to unmount"
+    exit 
+fi
+
+export MY_BUCKET=$1
+
+fusermount -u /home/jupyter/gcs/$MY_BUCKET
+rmdir /home/jupyter/gcs/$MY_BUCKET
+
+echo "removed dir  /home/jupyter/gcs/${MY_BUCKET}"
+echo "${MY_BUCKET} unmounted with /home/jupyter/gcs/${MY_BUCKET}"
